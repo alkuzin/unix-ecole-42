4f6c2f683ced5f08c66f0648a116399034f136c7
alkuzin <alkuzindev@gmail.com>
2023-11-19 13:55:57 +0300

    Added libft/libft.h

---
+++ libft/libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 16:07:40 by alkuzin           #+#    #+#             */
+/*   Updated: 2023/11/16 18:46:30 by alkuzin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "stdio/ft_stdio.h"
+#include "ctype/ft_ctype.h"
+#include "memory/ft_memory.h"
+#include "string/ft_string.h"
+#include "stdlib/ft_stdlib.h"
+
+#endif /* LIBFT_H */
